c6b8d5f24a47dc081cd9212c10b4a55f2614ca8b
cauaalves <cauaalves.dev@gmail.com>
2026-04-09 00:52:36 -0300

    fix: migrate eslint config to native flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,22 @@
+import js from "@eslint/js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-const config = [...compat.extends("next/core-web-vitals")];
+/** @type {import("eslint").Linter.Config[]} */
+const config = [
+  js.configs.recommended,
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
+];
 
 export default config;
