0185b83f854234044217daeb1938218b678f0496
cauaalves <cauaalves.dev@gmail.com>
2026-04-09 01:31:59 -0300

    fix: upgrade next.js to patched version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,12 @@
 import js from "@eslint/js";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import nextPlugin from "@next/eslint-plugin-next";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 /** @type {import("eslint").Linter.Config[]} */
 const config = [
   js.configs.recommended,
   {
-    plugins: {
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-    },
+    plugins: { "@next/next": nextPlugin },
+    rules: { ...nextPlugin.configs.recommended.rules },
   },
 ];
 
